b3a00ef7dbe60bf7de5341de615e1a63d9e2056f
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2023-12-18 16:09:31 -0300

    adding city and state in db at reset_database() function

--- databases/utils.py
+++ databases/utils.py
@@ -1,4 +1,4 @@
-from .sql_db import engine, Base
+from .sql_db import engine, Base, session
 from asyncio import run
 
 from apps.user.models import *
@@ -8,6 +8,7 @@ from apps.order.models import *
 
 def reset_database():
     run(drop_create_database())
+    run(create_city_state())
 
 
 async def drop_create_database():
@@ -16,3 +17,13 @@ async def drop_create_database():
         await conn.run_sync(Base.metadata.create_all)
 
 
+async def create_city_state():
+    async with session() as s:
+        state = Uf(abbreviation="PA", name="Philadelphia")
+        s.add(state)
+
+        await s.commit()
+        city = City(name="Scranton", uf_id=state.id)
+        s.add(city)
+
+        await s.commit()
